b4f66e387bf8219e50d6a35d89e77cccf78ec1e4
Smart Spaces Dev <dev@smartspacesgroup.com>
2025-07-25 17:45:27 -0400

    Version31

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
