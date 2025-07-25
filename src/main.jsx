e06b6edcb8d2cec6e1269b71e33be12c39fdbf95
Smart Spaces Dev <dev@smartspacesgroup.com>
2025-07-25 17:02:10 -0400

    Version18

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import DeviceTreeUploader from './DeviceTreeUploader';
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import DeviceTreeUploader from "./DeviceTreeUploader";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <DeviceTreeUploader />
   </React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
