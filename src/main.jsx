fb9477b131ee46b073170f0b52920eeecbe1a6d4
Smart Spaces Dev <dev@smartspacesgroup.com>
2025-07-25 17:21:22 -0400

    Version23

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import DeviceTreeUploader from './DeviceTreeUploader'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <DeviceTreeUploader />
-  </React.StrictMode>
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
