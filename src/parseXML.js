b46d907a6e76944c949ce8e281e81971573c6033
Smart Spaces Dev <dev@smartspacesgroup.com>
2025-07-25 18:40:22 -0400

    Version48

--- src/parseXML.js
+++
@@ -1,9 +0,0 @@
-
-export async function parseXML(content) {
-  const fastXmlParser = await eval("import('fast-xml-parser')");
-  const parser = new fastXmlParser.XMLParser({
-    ignoreAttributes: false,
-    attributeNamePrefix: "@_",
-  });
-  return parser.parse(content);
-}
